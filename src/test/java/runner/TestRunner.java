a1ce63541925217758102d21032c806d3cc3ccd5
vargas44 <ml_44@live.com.ar>
2024-10-14 15:51:29 -0300

    E2E

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -17,7 +17,7 @@ import org.junit.runners.Suite;
         features = {"src/main/resources/features/PlataformaIntegracion"
         },
         glue = {"stepsDefinitions", "framework","PlataformaIntegracion","Google","MiCorreo1_5","Presta","Shopify"}, // Agrega el paquete de los hooks y framework
-        tags =  "@Woocommerce"
+        tags =  "@PPDIPI-3236"
         //"@PPDIPI-3167"
 )
 public class TestRunner {
